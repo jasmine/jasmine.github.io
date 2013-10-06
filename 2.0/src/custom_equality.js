34ce33ee06ea2769b5ac6e430d1b05a02ec6093d
slackersoft <gregg@slackersoft.net>
2013-10-06 13:26:03 -0700

    Rework custom equality page so it doesn't break the introduction
    - When run altogether in jasmine-core CI
    
    [#54582648]

--- 2.0/src/custom_equality.js
+++ 2.0/src/custom_equality.js
@@ -1,37 +1,42 @@
 /**
- * ## Custom Equality Matchers
- * You can customize how jasmine determines if two objects are equal by defining your own custom equality matchers.
- * A custom equality matcher is a function that takes two arguments.
+ * ## Custom Equality Testers
  */
-var myCustomEquality = function(first, second) {
+describe("custom equality", function() {
   /**
-   * If the custom equality matcher knows how to compare the two items, it should return either true or false
+   * You can customize how jasmine determines if two objects are equal by defining your own custom equality testers.
+   * A custom equality tester is a function that takes two arguments.
    */
+  var myCustomEquality = function(first, second) {
+    /**
+     * If the custom equality tester knows how to compare the two items, it should return either true or false
+     */
 
-  if (typeof first == "string" && typeof second == "string") {
-    return first[0] == second[1];
-  }
+    if (typeof first == "string" && typeof second == "string") {
+      return first[0] == second[1];
+    }
+
+    /**
+     * Otherwise, it should return undefined, to tell jasmine's equality tester that it can't compare the items
+     */
+  };
 
   /**
-   * Otherwise, it should return undefined, to tell jasmine's equality tester that it can't compare the items
+   * Then you register your tester in a `beforeEach` so jasmine knows about it.
    */
-};
+  beforeEach(function() {
+    jasmine.addCustomEqualityTester(myCustomEquality);
+  });
 
-/**
- * Then you register your matcher so jasmine knows about it.
- */
-beforeEach(function() {
-  jasmine.addCustomEqualityTester(myCustomEquality);
-});
-
-/**
- * Then when you do comparisons in a spec, custom equality matchers will be checked first before the default equality logic.
- */
-describe("with custom equality", function() {
+  /**
+   * Then when you do comparisons in a spec, custom equality testers will be checked first before the default equality logic.
+   */
   it("should be custom equal", function() {
     expect("abc").toEqual("aaa");
   });
 
+  /**
+   * If your custom tester returns false, no other equality checking will be done.
+   */
   it("should be custom not equal", function() {
     expect("abc").not.toEqual("abc");
   });
