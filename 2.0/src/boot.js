2af38af73da82d5534de0c585dcc813f99677e87
Davis W. Frank <dwfrank+github@infe.ws>
2013-10-10 14:04:21 -0700

    Upgrading to latest Jasmine HEAD

--- 2.0/src/boot.js
+++ 2.0/src/boot.js
@@ -1,12 +1,12 @@
 /**
-Starting with version 2.0, this file "boots" Jasmine, performing all of the necessary initialization before executing the loaded environment and all of a project's specs. This file should be loaded after `jasmine.js`, but before any project source files or spec files are loaded. Thus this file can also be used to customize Jasmine for a project.
+ Starting with version 2.0, this file "boots" Jasmine, performing all of the necessary initialization before executing the loaded environment and all of a project's specs. This file should be loaded after `jasmine.js`, but before any project source files or spec files are loaded. Thus this file can also be used to customize Jasmine for a project.
 
-If a project is using Jasmine via the standalone distribution, this file can be customized directly. If a project is using Jasmine via the [Ruby gem][jasmine-gem], this file can be copied into the support directory via `jasmine copy_boot_js`. Other environments (e.g., Python) will have different mechanisms.
+ If a project is using Jasmine via the standalone distribution, this file can be customized directly. If a project is using Jasmine via the [Ruby gem][jasmine-gem], this file can be copied into the support directory via `jasmine copy_boot_js`. Other environments (e.g., Python) will have different mechanisms.
 
-The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
+ The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
 
-[jasmine-gem]: http://github.com/pivotal/jasmine-gem
-*/
+ [jasmine-gem]: http://github.com/pivotal/jasmine-gem
+ */
 
 (function() {
 
@@ -85,7 +85,7 @@ The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
   };
 
   /**
-   * Add all of the Jasmine public interface to the proper global, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
+   * Add all of the Jasmine global/public interface to the proper global, so a project can use the public interface directly. For example, calling `describe` in specs instead of `jasmine.getEnv().describe`.
    */
   if (typeof window == "undefined" && typeof exports == "object") {
     extend(exports, jasmineInterface);
@@ -93,11 +93,25 @@ The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
     extend(window, jasmineInterface);
   }
 
-/**
- * ## Runner Parameters
- *
- * More browser specifc code - wrap the query string in an object and to allow for getting/setting parameters from the runner user interface.
- */
+  /**
+   * Expose the interface for adding custom equality testers.
+   */
+  jasmine.addCustomEqualityTester = function(tester) {
+    env.addCustomEqualityTester(tester);
+  };
+
+  /**
+   * Expose the interface for adding custom expectation matchers
+   */
+  jasmine.addMatchers = function(matchers) {
+    return env.addMatchers(matchers);
+  };
+
+  /**
+   * ## Runner Parameters
+   *
+   * More browser specific code - wrap the query string in an object and to allow for getting/setting parameters from the runner user interface.
+   */
 
   var queryString = new jasmine.QueryString({
     getWindowLocation: function() { return window.location; }
@@ -121,7 +135,7 @@ The location of `boot.js` can be specified and/or overridden in `jasmine.yml`.
   });
 
   /**
-   * The `jsApiReporter` also receives spec results, and is used by any enivorment that needs to extract the results  from JavaScript.
+   * The `jsApiReporter` also receives spec results, and is used by any environment that needs to extract the results  from JavaScript.
    */
   env.addReporter(jasmineInterface.jsApiReporter);
   env.addReporter(htmlReporter);
