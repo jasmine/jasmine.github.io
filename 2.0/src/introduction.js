a125dd91455d64221407ee9b3d2fa9467966e446
Sheel Choksi <sheelchoksi@gmail.com>
2014-02-02 18:51:17 -0800

    Document how the 'this' keyword can be used to share state
    As discussed in pivotal/jasmine#513

--- 2.0/src/introduction.js
+++ 2.0/src/introduction.js
@@ -238,6 +238,26 @@ describe("A spec (with setup and tear-down)", function() {
 });
 
 /**
+### The `this` keyword
+Another way to share variables between a `beforeEach`, `it`, and `afterEach` is through the `this` keyword. Each spec's `beforeEach`/`it`/`afterEach` has the `this` as the same empty object that is set back to empty for the next spec's `beforeEach`/`it`/`afterEach`.
+*/
+describe("A spec", function() {
+  beforeEach(function() {
+    this.foo = 0;
+  });
+
+  it("can use the `this` to share state", function() {
+    expect(this.foo).toEqual(0);
+    this.bar = "test pollution?";
+  });
+
+  it("prevents test pollution by having an empty `this` created for the next spec", function() {
+    expect(this.foo).toEqual(0);
+    expect(this.bar).toBe(undefined);
+  });
+});
+
+/**
  ### Nesting `describe` Blocks
  Calls to `describe` can be nested, with specs defined at any level. This allows a suite to be composed as a tree of functions. Before a spec is executed, Jasmine walks down the tree executing each `beforeEach` function in order. After the spec is executed, Jasmine walks through the `afterEach` functions similarly.
  */
