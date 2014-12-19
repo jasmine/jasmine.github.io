4d43c625aa0226b9d8f35ed02bd2a53f1f17a3f7
slackersoft <gregg@slackersoft.net>
2014-12-19 12:47:45 -0800

    Add docs for `stringMatching`
    [finish #58120558]

--- edge/src/introduction.js
+++ edge/src/introduction.js
@@ -850,6 +850,28 @@ describe("jasmine.objectContaining", function() {
   });
 });
 
+/**
+ ## String Matching with `jasmine.stringMatching`
+ `jasmine.stringMatching` is for when you don't want to match a string in a larger object exactly, or match a portion of a string in a spy expectation.
+ */
+describe('jasmine.stringMatching', function() {
+  it("matches as a regexp", function() {
+    expect({foo: 'bar'}).toEqual({foo: jasmine.stringMatching(/^bar$/)});
+    expect({foo: 'foobarbaz'}).toEqual({foo: jasmine.stringMatching('bar')});
+  });
+
+  describe("when used with a spy", function() {
+    it("is useful for comparing arguments", function() {
+      var callback = jasmine.createSpy('callback');
+
+      callback('foobarbaz');
+
+      expect(callback).toHaveBeenCalledWith(jasmine.stringMatching('bar'));
+      expect(callback).not.toHaveBeenCalledWith(jasmine.stringMatching(/^bar$/));
+    });
+  });
+});
+
 
 /**
  ## Jasmine Clock
