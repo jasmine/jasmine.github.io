71a1607cf2eaf04ad8decbd38202202bd6ca9f10
slackersoft <gregg@slackersoft.net>
2015-01-14 17:24:29 -0800

    Add some docs for custom asymmetricMatch
    [finish #86141284]

--- edge/src/introduction.js
+++ edge/src/introduction.js
@@ -901,6 +901,33 @@ describe('jasmine.stringMatching', function() {
   });
 });
 
+/**
+ ## Custom asymmetric equality tester
+ When you need to check that something meets a certain criteria, without being strictly equal, you can also specify a custom asymmetric equality tester simply by providing an object that has an `asymmetricMatch` function.
+ */
+describe("custom asymmetry", function() {
+  var tester = {
+    asymmetricMatch: function(actual) {
+      var secondValue = actual.split(',')[1];
+      return secondValue === 'bar';
+    }
+  };
+
+  it("dives in deep", function() {
+    expect("foo,bar,baz,quux").toEqual(tester);
+  });
+
+  describe("when used with a spy", function() {
+    it("is useful for comparing arguments", function() {
+      var callback = jasmine.createSpy('callback');
+
+      callback('foo,bar,baz');
+
+      expect(callback).toHaveBeenCalledWith(tester);
+    });
+  });
+});
+
 
 /**
  ## Jasmine Clock
