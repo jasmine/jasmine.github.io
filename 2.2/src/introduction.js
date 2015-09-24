deb8ceed3d35bdd253a26b61ac428d30669311ad
Gregg Van Hove <gregg@slackersoft.net>
2015-09-24 13:13:15 -0700

    Update async docs to better describe the current behavior upon timeout.
    jasmine/jasmine#807

--- 2.2/src/introduction.js
+++ 2.2/src/introduction.js
@@ -1045,6 +1045,8 @@ describe("Asynchronous specs", function() {
 
   /**
    By default jasmine will wait for 5 seconds for an asynchronous spec to finish before causing a timeout failure.
+   If the timeout expires before `done` is called, the current spec will be marked as failed and suite execution will continue as if `done` was called.
+
    If specific specs should fail faster or need more time this can be adjusted by passing a timeout value to `it`, etc.
 
    If the entire suite should have a different timeout, `jasmine.DEFAULT_TIMEOUT_INTERVAL` can be set globally, outside of any given `describe`.
