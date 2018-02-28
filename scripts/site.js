830d9cd48e0c591fcb5a5b87f66a788e351fb871
Gregg Van Hove <gvanhove@pivotal.io>
2018-02-28 14:05:42 -0800

    add small header back so the menu sticks too

--- scripts/site.js
+++ scripts/site.js
@@ -6,11 +6,16 @@
 
       document.addEventListener('scroll', function() {
         header.classList.remove('shrinking');
+        header.classList.remove('small');
 
         var height = Math.max(50, 300 - window.scrollY);
         header.style.height = height + 'px';
         if (height <= 150) {
           header.classList.add('shrinking');
+        }
+
+        if (height === 50) {
+          header.classList.add('small');
         }
       });
     },
