99da721ed728a55887110f3633a83ef4d0c69666
Elliot Nelson <Elliot.Nelson@hbo.com>
2019-06-05 06:31:02 -0400

    Avoid covering up target header with the navigation bar

--- scripts/site.js
+++ scripts/site.js
@@ -29,9 +29,25 @@
       });
     },
 
+    scrollTargetLink: function scrollTargetLink() {
+      // If there is a :target (hash tag pointing to a specific section of this
+      // article), scroll the page slightly so that the target header is not
+      // covered by the floating navbar.
+      //
+      // There are various tricks for doing this using CSS ::before selectors,
+      // but as of June 2019, none of them work in Chrome.
+      var html = document.getElementsByTagName('html')[0];
+      var target = document.querySelector(':target');
+      if (target) {
+        html.scrollTo(0, target.offsetTop - 64);
+      }
+    },
+
     init: function init() {
       window.JasmineDocs.initHeader();
       window.JasmineDocs.initSearch();
+      window.addEventListener('load', window.JasmineDocs.scrollTargetLink);
+      window.addEventListener('hashchange', window.JasmineDocs.scrollTargetLink);
     }
   };
 })();
