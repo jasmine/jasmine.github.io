3aa4977e89bd1319399592539ae864059360d34a
Steve Gravrock <sdg@panix.com>
2024-11-02 10:22:27 -0700

    Fixed behavior of links in docco tutorials

--- scripts/site.js
+++ scripts/site.js
@@ -9,24 +9,9 @@
       });
     },
 
-    scrollTargetLink: function scrollTargetLink() {
-      // If there is a :target (hash tag pointing to a specific section of this
-      // article), scroll the page slightly so that the target header is not
-      // covered by the floating navbar.
-      //
-      // There are various tricks for doing this using CSS ::before selectors,
-      // but as of June 2019, none of them work in Chrome.
-      var target = document.querySelector(':target');
-      if (target) {
-        document.querySelector('body').scrollTo(0, target.offsetTop - 64);
-        document.querySelector('html').scrollTo(0, target.offsetTop - 64);
-      }
-    },
-
     init: function init() {
       window.JasmineDocs.initSearch();
       window.addEventListener('load', window.JasmineDocs.scrollTargetLink);
-      window.addEventListener('hashchange', window.JasmineDocs.scrollTargetLink);
       anchors.add('.article h1:not(.no_toc),.article h2:not(.no_toc),.article h3:not(.no_toc),.article h4:not(.no_toc)');
     }
   };
