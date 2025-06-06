7e23b057e3da657417a7561ce69342eb438f72b7
Steve Gravrock <sdg@panix.com>
2025-06-06 07:02:44 -0700

    Fix links to custom matcher tutorial

--- _jsdoc_plugins/see_tutorials.js
+++ _jsdoc_plugins/see_tutorials.js
@@ -1,7 +1,52 @@
-var fs = require('fs');
+const fs = require('fs');
+const path = require('path');
+const YAML = require('yaml');
+
+let tutorials;
 
 function tutorialExists(text) {
-  return fs.existsSync("_tutorials/" + text + ".html") || fs.existsSync("_tutorials/" + text + ".md");
+  loadTutorials(text);
+  return tutorials.includes(text);
+}
+
+function loadTutorials() {
+  if (tutorials) {
+    return;
+  }
+
+  const result = [];
+  const exts = /\.(md|html)$/;
+  const tutorialPathPattern = /^\/tutorials\//;
+
+  for (const fn of fs.readdirSync('_tutorials/')) {
+    if (fn.match(exts)) {
+      result.push(fn.replace(exts, ''));
+
+      const fullPath = path.join('_tutorials/', fn);
+      const text = fs.readFileSync(fullPath, {encoding: 'utf8'});
+      const doc = YAML.parseAllDocuments(text)[0];
+      let redirects = doc.contents.get('redirect_from');
+
+      if (redirects) {
+        if (typeof redirects === 'string') {
+          redirects = [redirects];
+        } else if (redirects.items) {
+          redirects = redirects.items.map(item => item.value);
+        } else {
+          throw new Error('Expected redirect_from to be a string or YAMLSeq ' +
+              `but it was a ${typeof redirects} ( in ${fn}`)
+        }
+
+        for (const r of redirects) {
+          if (r.match(tutorialPathPattern)) {
+            result.push(r.replace(tutorialPathPattern, ''));
+          }
+        }
+      }
+    }
+  }
+
+  tutorials = result;
 }
 
 exports.handlers = {
